3122924f1622561baf776ffcfd402aac9072657a
Vignesh <vigneshkumarg18@gmail.com>
2022-12-28 22:59:21 +0530

    Basic io

---
+++ 1_Learn_the_Basics/1_1_Language_Basics/basic_io.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cout << "Enter A : ";
+    cin >> x;
+    cout << "Enter B : ";
+    cin >> y;
+    cout << "Total : " << x + y << endl;
+    return 0;
+}
